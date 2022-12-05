668bcc28439935bb91a89185d0fb9755f55d7c69
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2022-12-05 10:59:03 +0600

    simplified day 4 code

--- 2022/Day04/Day04.fsx
+++ 2022/Day04/Day04.fsx
@@ -1,35 +1,27 @@
 let tupleApply f (a, b) = f a, f b
 
 ///"2-4,6-8" -> ("2-4", "6-8")
-let makePair (str: string) =
+let parsePair (str: string) =
     match str.Split(",") with
     | [| x; y |] -> (x, y)
-    | _ -> failwith "Unreachable"
+    | _ -> failwith "unhandled!"
 
 ///"2-4" -> Set [2; 3; 4]
-let makeRange (str: string) =
+let parseRange (str: string) =
     match str.Split("-") with
     | [| x; y |] -> Set [ int x .. int y ]
-    | _ -> failwith "Unreachable"
+    | _ -> failwith "unhandled!"
 
-let parsePair = makePair >> tupleApply makeRange
+let parseLine = parsePair >> tupleApply parseRange
 
-let input = System.IO.File.ReadLines "input.txt" |> Seq.map parsePair
+let lengthBy fn = Seq.filter fn >> Seq.length
+
+let input = System.IO.File.ReadLines "input.txt" |> Seq.map parseLine
 
 // Part I
-
 let isSubset (x, y) = Set.isSubset x y || Set.isSubset y x
-
-///Filter range pairs that fully contains the other
-let subsettingRanges = Seq.filter isSubset
-
-printfn "Number of Elf assignment pairs that fully contain the other: %d" (input |> subsettingRanges |> Seq.length)
+printfn "Number of Elf assignment pairs that fully contain the other: %d" (input |> lengthBy isSubset)
 
 // Part II
-
 let isIntersect (x, y) = Set.intersect x y <> Set.empty
-
-///Filter range pairs that overlap one another
-let intersectingRanges = Seq.filter isIntersect
-
-printfn "Number of Elf assignment pairs that overlap: %d" (input |> intersectingRanges |> Seq.length)
+printfn "Number of Elf assignment pairs that overlap: %d" (input |> lengthBy isIntersect)
